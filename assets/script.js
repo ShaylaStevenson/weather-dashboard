672f14b1b7f4997f45d036482c8b9f4ee2476e29
Shayla Stevenson <74476502+Shazzyshay@users.noreply.github.com>
2021-02-02 11:05:13 -0800

    page links and todo list

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+//Todos
+//Use form inputs to create HTML page
+//Display current and future conditions
+//Search history list
+//Current weather includes: city name, date, icon, temperature, humidity, windspeed, uv index
+//uv index changes color for favorable, moderate, severe
+//future weather includes: 5 days, date, icon, temp, humidity
+//When I click on search history item, get current and future weather
+
